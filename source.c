4314ed4d5d6458706f71d07c6481e9aeb1f45d3b
stankovicuros27 <stankovicuros27@gmail.com>
2020-04-22 17:35:37 +0200

    Tested trig operations and prefix notation

--- source.c
+++ source.c
@@ -53,8 +53,6 @@ const char* chstack_peek(struct CharStack* stack) {
 }
 //-----Character Stack End-----
 
-
-
 int is_operand(char* ch) {
 	if (strlen(ch) > 2) return 0;
 	if (ch[0] >= 'A' && ch[0] <= 'Z') return 1;
@@ -65,8 +63,8 @@ int get_priority(char* operation) {
 	if (strcmp(operation, "+") == 0 || strcmp(operation, "-") == 0) return 1;
 	if (strcmp(operation, "*") == 0 || strcmp(operation, "/") == 0) return 2;
 	if (strcmp(operation, "^") == 0) return 3;
-	if (strcmp(operation, "ln") == 0 || strcmp(operation, "tg") == 0) return 4;
-	if (strcmp(operation, "(-)") == 0) return 4;
+	if (strcmp(operation, "ln") == 0 || strcmp(operation, "tg") == 0 || strcmp(operation, "sin") == 0 || strcmp(operation, "cos") == 0) return 4;
+	if (strcmp(operation, "(-)") == 0 || strcmp(operation, "abs") == 0) return 4;
 	return -1;
 }
 
@@ -74,8 +72,8 @@ int is_operation(char* operation) {
 	if (strcmp(operation, "+") == 0 || strcmp(operation, "-") == 0) return 1;
 	if (strcmp(operation, "*") == 0 || strcmp(operation, "/") == 0) return 2;
 	if (strcmp(operation, "^") == 0) return 3;
-	if (strcmp(operation, "ln") == 0 || strcmp(operation, "tg") == 0) return 4;
-	if (strcmp(operation, "(-)") == 0) return 4;
+	if (strcmp(operation, "ln") == 0 || strcmp(operation, "tg") == 0 || strcmp(operation, "sin") == 0 || strcmp(operation, "cos") == 0) return 4;
+	if (strcmp(operation, "(-)") == 0 || strcmp(operation, "abs") == 0) return 4;
 	return -1;
 }
 
@@ -85,6 +83,7 @@ int get_rang(char* operation) {
 	if (strcmp(operation, "^") == 0) return 1;
 	if (strcmp(operation, "ln") == 0) return 0;
 	if (strcmp(operation, "(-)") == 0 || strcmp(operation, "tg") == 0) return 0;
+	if (strcmp(operation, "sin") == 0 || strcmp(operation, "cos") == 0 || strcmp(operation, "abs")) return 0;
 	return 0;
 }
 
@@ -93,7 +92,7 @@ int get_rang(char* operation) {
 //-----Tree and Nodes-----
 struct Node {
 	char sym[5];
-	int val;
+	double val;
 	struct Node* left, * right, *parent;
 };
 
@@ -150,6 +149,9 @@ const char* ndstack_peek(struct NodeStack* stack) {
 	}
 	return stack->array[stack->top];
 }
+
+double calculate_value(struct Node* node);
+double val(struct Node* node);
 
 struct NodeQueue {
 	int front, rear, size;
@@ -213,10 +215,20 @@ struct Node* make_tree(char* postfix) {
 		else {
 			p = create_node(ptr);
 			t1 = ndstack_pop(stack);
-			t2 = ndstack_pop(stack);
+			if (get_rang(ptr) == 1) {
+				t2 = ndstack_pop(stack);
+			}
+			else t2 = NULL;
 
-			p->right = t1;
-			p->left = t2;
+			if (t2) {
+				p->right = t1;
+				p->left = t2;
+			}
+			else {
+				p->right = t2;
+				p->left = t1;
+			}
+
 
 			ndstack_push(stack, p);
 		}
@@ -263,7 +275,7 @@ void pretty_print_subtree(struct Node* node, char* prefix) {
 		strcpy(newPrefix, prefix);
 		strcat(newPrefix, (printStrand ? "|     " : "	 "));
 		printf("%s\n", node->right->sym);
-		pretty_print_subtree(node->right, newPrefix);
+		pretty_print_subtree(node->right, strcat(newPrefix, "	"));
 	}
 
 	if (hasLeft) {
@@ -288,16 +300,85 @@ void levelorder(struct Node* node) {
 			printf("\n");
 			continue;
 		}
-		printf("%s", p->sym);
-		if (is_operand(p->sym)) printf("(%d)", p->val);
-		printf(" ");
+		printf("%s ", p->sym);
 
 		if (p->left) ndqueue_insert(queue, p->left);
 		if (p->right) ndqueue_insert(queue, p->right);
 	}
 }
 
-void update_tree_values(struct Node* node, char* sym, int val) {
+void postorder(struct Node* node) {
+	if (node == NULL) return;
+	struct NodeStack* stack = create_node_stack(MAX_CAP);
+
+	do {
+		while (node) {
+			if (node->right)
+				ndstack_push(stack, node->right);
+			ndstack_push(stack, node);
+			node = node->left;
+		}
+
+		node = ndstack_pop(stack);
+		if (node->right && ndstack_peek(stack) == node->right) {
+			ndstack_pop(stack);
+			ndstack_push(stack, node);
+			node = node->right;
+		}
+		else {
+			printf("%s ", node->sym);
+			node = NULL;
+		}
+
+	} while (!ndstack_is_empty(stack));
+}
+
+double calculate_value(struct Node* node) {
+
+	if (is_operand(node->sym)) return node->val;
+
+	if (is_operation(node->sym)) {
+		if (strcmp(node->sym, "+") == 0) {
+			return val(node->left) + val (node->right);
+		} 
+		else if (strcmp(node->sym, "-") == 0) {
+			return val(node->left) - val(node->right);
+		}
+		else if (strcmp(node->sym, "*") == 0) {
+			return val(node->left) * val(node->right);
+		}
+		else if (strcmp(node->sym, "/") == 0) {
+			return val(node->left) / val(node->right);
+		}
+		else if (strcmp(node->sym, "^") == 0) {
+			return pow(val(node->left), val(node->right));
+		}
+		else if (strcmp(node->sym, "ln") == 0) {
+			return log(val(node->left));
+		}
+		else if (strcmp(node->sym, "tg") == 0) {
+			return tan(val(node->left));
+		}
+		else if (strcmp(node->sym, "(-)") == 0) {
+			return -val(node->left);
+		}
+		else if (strcmp(node->sym, "sin") == 0) {
+			return sin(val( node->left));
+		}
+		else if (strcmp(node->sym, "cos") == 0) {
+			return cos(val(node->left));
+		}
+		else if (strcmp(node->sym, "abs") == 0) {
+			if (val(node->left) < 0) return -val(node->left);
+			return val(node->left);
+		}
+	}
+	printf("Invalid tree calculation!");
+	exit(1);
+}
+
+
+void update_tree_values(struct Node* node, char* sym, double val) {
 	struct NodeQueue* queue = create_node_queue(MAX_CAP);
 	ndqueue_insert(queue, node);
 	ndqueue_insert(queue, NULL);
@@ -315,11 +396,42 @@ void update_tree_values(struct Node* node, char* sym, int val) {
 		if (p->right) ndqueue_insert(queue, p->right);
 	}
 }
+
+void enter_operand_values(struct Node* root, char** chArr) {
+
+	int i = 0;
+	while (chArr[i]) {
+		printf("Enter val of %s :", chArr[i]);
+		double k;
+		scanf("%lf", &k);
+		update_tree_values(root, chArr[i], k);
+		printf("\n");
+		i++;
+	}
+	printf("\n\nTree node values updated: \n");
+}
+double val(struct Node* node) { return calculate_value(node); }
+
+void delete_tree(struct Node* node) {
+	if (node == NULL) return;
+	struct NodeQueue* queue = create_node_queue(MAX_CAP);
+	ndqueue_insert(queue, node);
+	struct Node* p = NULL;
+
+	while (!ndqueue_is_empty(queue)) {
+		p = ndqueue_delete(queue);
+		if (p->left) ndqueue_insert(queue, p->left);
+		if (p->right) ndqueue_insert(queue, p->right);
+		printf("%s deleted\n", p->sym);
+		free (p);
+	}
+	node = NULL;
+}
+
 //-----Tree and Nodes End-----
 
 
-
-char* infix_to_postfix(char* expression, char** operands) {
+char* expr_wrapper(char* expression, char** operands) {
 	int i = -1;
 	int rang = 0;
 
@@ -344,12 +456,12 @@ char* infix_to_postfix(char* expression, char** operands) {
 					break;
 				}
 			}
-	
 			if (!present) {
 				char* tmp = malloc(MAX_CAP * (sizeof(char)));
 				strcpy(tmp, ptr);
 				strcpy(operands[++i], tmp);
 			} 
+
 			rang++;
 			strcat(res, ptr);
 			strcat(res, " ");
@@ -396,23 +508,146 @@ char* infix_to_postfix(char* expression, char** operands) {
 	return res;
 }
 
-void enter_operand_values(struct Node *root, char** chArr) {
-
-	int i = 0;
-	while (chArr[i]) {
-		printf("Enter val of %s :", chArr[i]);
-		int k;
-		scanf("%d", &k);
-		update_tree_values(root, chArr[i], k);
-		printf("\n");
-		i++;
-	}
-	printf("\n\nTree node values updated: \n");
-	levelorder(root);
+void console_output_start() {
+	printf("Welcome to Expression_Tree application!\n\n");
+	printf("Using this you can evaluate mathematical expressions using binary tree!\n");
+	printf("Uppercase letters are reserved for operands\nLowercase letters are reserved for functions\nSymbols are reserved for operations\n");
+	printf("Additionally, unary - operator is coded with \'(-)\'\n");
+	printf("\nAny valid combination of operators, functions and operands is allowed\n");
+	printf("Any invalid combination of operators, functions and operands leads to program termination!\n\n");
+	printf("Enter any symbol to continue!");
+	char c[MAX_CAP];
+	scanf("%s", c);
+	printf("\n");
 }
 
+void console_output() {
+	printf("Available commands:\n\n");
+	printf("1.	Create binary tree from expression\n");
+	printf("2.	Print binary tree (pretty print)\n");
+	printf("3.	Print binary tree (modified level order)\n");
+	printf("4.	Print prefix notation of given expression\n");
+	printf("5.	Enter operand values\n");
+	printf("6.	Calculate expression\n");
+	printf("7.	Delete existing tree (required for new expression)\n");
+	printf("0.	Exit program\n");
+	printf("\n\n");
+	printf("Enter command: ");
+}
+
+
 int main() {
+
+	console_output_start();
+
+	struct Node* root = NULL;
+	int menu;
+	char res[MAX_CAP];
 	char expression[MAX_CAP];
+	char util[MAX_CAP];
+	char** operands = NULL;
+
+	do {
+		console_output();
+		scanf("%d", &menu);
+		printf("\n\n");
+
+		switch (menu) {
+		case 1:
+		{
+			if (root) {
+				printf("Tree already exists! Delete it first to enter new expression!\n\n");
+				break;
+			}
+			
+			printf("Enter mathematical expression(c/p not supported, expression must be typed):\n");
+			char* util2 = fgets(util, MAX_CAP, stdin);
+			char* str_test = fgets(expression, MAX_CAP, stdin);
+			if (!str_test) {
+				printf("Invalid expression! Terminating program...");
+				exit(1);
+			}
+			expression[strcspn(expression, "\n")] = 0;
+			printf("You entered:\n%s\n\n", expression);
+			operands = (char**)malloc(MAX_CAP * sizeof(char*));
+			for (int q = 0; q < MAX_CAP; q++) {
+				operands[q] = (char*)malloc(MAX_CAP * sizeof(char));
+			}
+
+			strcpy(res, expr_wrapper(expression, operands));
+			root = make_tree(res);
+			printf("Tree has been made!\n\n");
+			break; 
+		}
+		case 2:
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			printf("Pretty print:\n\n");
+			pretty_print_tree(root);
+			printf("\n\n");
+			break;
+		case 3:
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			printf("Modified level order:\n\n");
+			levelorder(root);
+			printf("\n\n");
+			break;
+		case 4:
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			printf("Prefix notation:\n");
+			postorder(root);
+			printf("\n\n");
+			break;
+		case 5:
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			enter_operand_values(root, operands);
+			printf("Operand values sucessfuly initialized! \n\n");
+			break;
+		case 6:
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			printf("Calculating value...\n");
+			printf("Final result: %lf\n\n", calculate_value(root));
+			break;
+		case 7:
+		{
+			if (!root) {
+				printf("Tree is not initialized!\n\n");
+				break;
+			}
+			printf("Deleting tree...\n");
+			delete_tree(root);
+			root = NULL;
+			printf("Tree sucessfuly deleted!\n\n");
+				break;
+		}
+		case 0:
+			exit(0);
+		default:
+			printf("Invalid command!\n");
+			break;
+		}
+
+	} while (menu != 0);
+
+
+
+
+
+	/*char expression[MAX_CAP];
 	printf("Enter mathematical expression:\n");
 
 	char* str_test = fgets(expression, MAX_CAP, stdin);
@@ -430,7 +665,7 @@ int main() {
 		operands[q] = (char*)malloc(MAX_CAP * sizeof(char));
 	}
 
-	strcpy(res, infix_to_postfix(expression, operands));
+	strcpy(res, expr_wrapper(expression, operands));
 	printf("Postfix : \n %s\n\n", res);
 
 	struct Node* root = make_tree(res);
@@ -440,4 +675,7 @@ int main() {
 	pretty_print_tree(root, 0);
 
 	enter_operand_values(root, operands);
+
+	printf("Calculate value:\n");
+	printf("%lf", calculate_value(root));*/
 }
